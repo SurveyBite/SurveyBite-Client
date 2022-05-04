36edff1a935508d2921d0f0368d665fdfb92b269
Rebecca Foltin <rebecca.s.foltin@gmail.com>
2022-05-04 16:42:23 -0400

    commit

---
+++ src/components/Homepage/homepage.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+// id = '#root'
+// class = '.home-page', '.form', '.about', '.write', '.research', '.contact'
+
+<div id='root'>;
+    <section class='hp-banner'> Eat the world, one bite at a time. </section>;
+    <div class='form'> Surveys can be even more FUN with SurveyBite </div>;
+        <div class='about'> About SurveyBite </div>
+        <div class='write'> How to Brainstorm Survey Ideas </div>
+        <div class='research'> The Science of Surveys </div>
+        <div class='contact'> Wanna Chat? Contact us! </div>
+
+<footer> Copyright 2022, All Rights Reserved, SurveyBite 
+    <ul>
+        <li> Facebook </li>
+        <li> Twitter </li>
+    </ul>
+</footer>
+</div>
