6034134cfe7404e295235f6019ce129f860f242f
jgiang15 <jgiang15@gmail.com>
2022-05-05 16:36:23 -0400

    added heroku url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://floating-lowlands-79433.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
